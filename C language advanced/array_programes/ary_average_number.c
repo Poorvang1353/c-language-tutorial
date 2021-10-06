ac583f4676db7f94effdff6f318e95665ee16c04
Poorvang <poorvangpatel13@gmail.com>
2021-10-06 21:59:06 +0530

    commit

--- C language advanced/array_programes/ary_average_number.c
+++ C language advanced/array_programes/ary_average_number.c
@@ -2,11 +2,11 @@
 int main()
 {
      int marks[10], i, n, sum = 0, average;
-     printf("Enter number of elements: ");
+     printf("How many subject you have: ");
      scanf("%d", &n);
      for(i=0; i<n; i++)
      {
-          printf("Enter %d number: ",i+1);
+          printf("Enter %d subject mark's: ",i+1);
           scanf("%d", &marks[i]);
           sum =sum+marks[i];
      }
--- C language basic/rough.c
+++ C language basic/rough.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
-#include<conio.h>
+int sum(int a, int b);
 int main()
 {
-    int a;
-    printf("Enter the any value: ");
-    scanf("%d",&a);
-    printf("The value is %d",a);
+    int c;
+    c = sum(5,6);
+    printf("The sum of c is %d",c);
     return 0;
+}
+
+int sum(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
 }
